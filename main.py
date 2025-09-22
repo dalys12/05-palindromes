79a06d4dd7e1c45d3cd961615b98b0f0a947b9ec
anadelys-benoit <anadelys.benoit@icloud.com>
2025-09-22 16:49:38 +0000

    teste palindrome

--- main.py
+++ main.py
@@ -1,18 +1,57 @@
+""" Vérifier si un mot ou une phrase est un palindrome ou pas"""
+
 #### Fonction secondaire
 
+def clean (x):
+    """Enlever espace, accent et majuscule"""
+    x = x.replace(" ","")
+    x = x.replace(",","")
+    x = x.replace ("'","")
+    x = x.replace("-","")
+    x = x.replace(":","")
+    x = x.replace("?","")
+    x = x.replace("!","")
+    x = x.replace("é","e")
+    x = x.replace("è","e")
+    x = x.replace("ë","e")
+    x = x.replace("ê","e")
+    x = x.replace("ô","o")
+    x = x.replace("à","a")
+    x = x.replace("ù","u")
+    x = x.replace("û","u")
+    x = x.replace("ç","c")
+    x = x.replace ("É","e")
+    x = x.replace ("À","a")
+    x = x.lower()
+    return x
 
-def ispalindrome(p):
 
-    # votre code ici
-    
-    return False
+def ispalindrome(s):
+    """ 
+    Vérifier si une chaine de caractère est un palindrome ou pas.
+    Args :
+        s : chaine de caractère
+    Returns :
+        booléens qui dit si s est un palindrome ou non
+    """
+    x = clean(s)
+    n = len(x)
+    if n % 2 == 0:
+        for i in range (n):
+            if x[i] != x[n-i-1]:
+                return False
+    else:
+        m = int(((n -1)/2) +1)
+        for i in range (m):
+            if x[i] != x[n-i-1]:
+                return False
+    return True
 
 #### Fonction principale
 
 
 def main():
-
-    # vos appels à la fonction secondaire ici
+    """test la fonction"""
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
